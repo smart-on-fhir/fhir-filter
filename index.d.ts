3aee04c56baaff6fb23908a1ab5719e7d6dea744
Vladimir Ignatov <vlad.ignatov@gmail.com>
2022-04-24 12:53:04 -0400

    Improved type definitions

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,12 @@
+declare function fhirFilter(item: JSONObject, index?: number, all?: JSONObject[]): JSONObject[]
+
+declare namespace fhirFilter {    
+    export const create: (filter: string) => ((item: JSONObject, index?: number, all?: JSONObject[]) => JSONObject[]);
+}
+
 export type JSONScalar = string | number | boolean | null;
 export type JSONArray  = JSONValue[];
 export type JSONObject = { [ key: string ]: JSONValue };
 export type JSONValue  = JSONScalar | JSONArray | JSONObject;
+
+export = fhirFilter;
